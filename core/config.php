f30cf14c9adbb62aeb30f9794fb0347aebfe4729
Som_Meher <som.meher.2024@gmail.com>
2026-02-03 14:24:45 +0530

    config change

--- core/config.php
+++ core/config.php
@@ -1,11 +1,29 @@
 <?php
 // core/config.php
 
-// Base URL for the project
-define("BASE_URL", "http://localhost/shopapp");
-
 /*
-Later on VPS:
-define("BASE_URL", "https://triveni.cloud/shop");
+    Smart BASE_URL Detection
+    Works on:
+    - Localhost (XAMPP)
+    - VPS Deployment (triveni.cloud/private/shopapp)
 */
+
+/* Detect Host */
+$host = $_SERVER["HTTP_HOST"];
+
+/* Default Path (Project Folder Name) */
+$projectFolder = "/shopapp";
+
+/* VPS Path */
+$vpsFolder = "/private/shopapp";
+
+/* Localhost Setup */
+if ($host === "localhost") {
+    define("BASE_URL", "http://localhost" . $projectFolder);
+}
+
+/* VPS Setup */
+else {
+    define("BASE_URL", "https://" . $host . $vpsFolder);
+}
 ?>
